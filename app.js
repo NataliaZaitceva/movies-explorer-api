73fd4dac14b392f26a8ebd747cc271c4317cf439
Natalia Zaitceva <sfk89@yandex.ru>
2023-01-30 11:54:19 +0900

    add url

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -8,16 +9,34 @@ const { MONGO_DEV_URL } = require('./config');
 const errorHandler = require('./middlewares/errorHandler');
 const limiter = require('./middlewares/rateLimit');
 
+const app = express();
+
 const { NODE_ENV, MONGO_URL } = process.env;
 
-const { PORT = 3000 } = process.env;
+const { PORT = 4000 } = process.env;
 
-const app = express();
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'http://mesto.students.nomoredomains.club',
+    'https://mesto.students.nomoredomains.club',
+    'https://github.com/NataliaZaitceva/react-mesto-api-full.git',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 mongoose.set('strictQuery', false);
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_DEV_URL);
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(requestLogger);
 app.use(helmet());
 app.use(limiter);
